740255fa3c7acf26fa0f90a01d7cfcd0c48abe7c
Liron Rotman <93140643+LER96@users.noreply.github.com>
2023-01-30 14:20:18 +0200

    Renew

--- DiceInt.cs
+++ DiceInt.cs
@@ -7,35 +7,33 @@ using GenericHW;
 class DiceInt : Dice<int>
 {
     public int Scalar { get; set; }
-    public int BaseDie { get; set; }
-    public int Modifier { get; set; }
+    public override int BaseDie { get; set; }
 
-    public DiceInt(int x, int y) : base(x, y)
+    public DiceInt(int x, int y) : base(y)
     {
-
+        this.Scalar = x;
     }
 
     public override int Roll()
     {
         int result = 0;
         Random rnd = new Random();
-        for (int i = 0; i < base.Scalar; i++)
+        for (int i = 0; i < this.Scalar; i++)
         {
             result += rnd.Next(1, base.BaseDie + 1);
         }
-        result += Modifier;
         return result;
     }
 
     public override string ToString()
     {
-        return $"Dice({Scalar}d,{BaseDie},{Modifier})";
+        return $"Dice({Scalar}d,{BaseDie})";
     }
 
     public override bool Equals(object? obj)
     {
         DiceInt d = (DiceInt)obj;
-        return Scalar == d.Scalar && BaseDie == d.BaseDie && Modifier == d.Modifier;
+        return Scalar == d.Scalar && BaseDie == d.BaseDie;
     }
 
     public override int GetHashCode()
